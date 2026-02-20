9f675b3e5ebdf4985654f30082b699e5c64755f7
Rineyar <72439847+Rineyar@users.noreply.github.com>
2026-02-20 23:21:56 +0700

    Сделал общую структуру, но её всю нужно переделать
    !!!НЕРАБОЧЕЕ!!!

--- generator.py
+++ generator.py
@@ -1,16 +1,114 @@
-from sklearn.datasets import make_classification
 import numpy as np
+from math import pow,sin,cos
 import matplotlib.pyplot as plt
-import pandas as pd
 
-len = 100
+def gen_func_line(func = None, length = 10, discretisation = 1, do_negative = False, return_data = False):
+    """
+    Генератор ряда на основе математический функции.\n
+    Поддерживается на данный момент:\n
+    Линейная - "linear" args = [коэфф.;свободный]\n
+    Степенная - "power" args = [степень;коэфф.;свободный]\n
+    Корень - "root" args = [степень;коэфф.;свободный]\n
+    Синусоида - "sin" args = [коэфф.;свободный]\n
+    Косинусоида - "cos" args = [коэфф.;свободный]\n
+    \n
+    func - словарь с ключами:\n
+    "type" - где хранится тип функции в строке\n
+    "args" - список аргументов к необходимой функции\n
+    \n
+    length - кол-во значений x обрабатываемых функцией\n
+    \n
+    discretisation - как сильно будет одно значение обрабатываться\n
+    \n
+    do_negative - будет ли обработка с отрицательных значений\n
+    \n
+    return_data - пока не готово\n
+    """
 
-def generate_dataline(len = 10,seed = 993):
-    x,y = make_classification(n_samples=1,n_features=len,n_informative=10,n_redundant=2,n_classes=2,random_state=seed)
-    dataline = pd.DataFrame(x)
-    return dataline
+    y = []
 
-dataline = generate_dataline(len,1)
+    if func == None:
+        raise ValueError("Не выбран тип функции")
+    
+    elif func["type"] == "linear":
+        a = func["args"][0]
+        b = func["args"][1]
 
-plt.hist(dataline,bins=len)
-plt.show()
+        if do_negative == True:
+            for x in range(length/2,length/2,1/discretisation):
+                y[x] = a*x+b
+        else:
+            for x in range(0,length,1):
+                y.append(a*x+b)
+
+    elif func["type"] == "power":
+        stepen = func["args"][0]
+        a = func["args"][1]
+        b = func["args"][2]
+
+        if do_negative == True:
+            for x in range(length/2,length/2,1/discretisation):
+                y[x] = a*pow(x,stepen)+b
+        else:
+            for x in range(0,length,1/discretisation):
+                y[x] = a*pow(x,stepen)+b
+
+    elif func["type"] == "root":
+        stepen = func["args"][0]
+        a = func["args"][1]
+        b = func["args"][2]
+
+        if do_negative == True:
+            for x in range(length/2,length/2,1/discretisation):
+                y[x] = a*pow(x,1/stepen)+b
+        else:
+            for x in range(0,length,1/discretisation):
+                y[x] = a*pow(x,1/stepen)+b
+
+    elif func["type"] == "sin":
+        a = func["args"][0]
+        b = func["args"][1]
+
+        if do_negative == True:
+            for x in range(length/12,length/12,1/(6*discretisation)):
+                y[x] = a*sin(x)+b
+        else:
+            for x in range(0,length/6,1/(6*discretisation)):
+                y[x] = a*sin(x)+b
+
+    elif func["type"] == "cos":
+        a = func["args"][0]
+        b = func["args"][1]
+
+        if do_negative == True:
+            for x in range(length/12,length/12,1/(6*discretisation)):
+                y[x] = a*cos(x)+b
+        else:
+            for x in range(0,length/6,1/(6*discretisation)):
+                y[x] = a*cos(x)+b
+
+    else:
+        raise ValueError("Некорректный тип функции")
+    
+    if return_data == False:
+        return y
+    else:
+        return y
+    
+def test():
+    func = {"type":"linear","args":[2,0]}
+    length = 100
+    discretisation = 10
+    
+    dataline = gen_func_line(func,length,discretisation)
+
+    plt.plot(dataline)
+    plt.show()
+
+test()
+
+#питон не умеет считать по нецелым, это печельно
+#убрал на тесте, потом что-нибудь придумать
+
+#и сразу в ячейку применять тоже
+#значит везде append проставить надо будет
