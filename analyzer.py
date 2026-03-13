e9e61e0feab84a7622e33f7cba30b62f002e2ec3
yarik <yor.rogov@ya.ru>
2026-03-13 15:27:33 +0700

    метод 3х сигм

--- analyzer.py
+++ analyzer.py
@@ -2,30 +2,32 @@ import numpy as np
 import pandas as pd
 
 def get_z_score(X):
-    
+    mean = np.nanmean(X)
+    std = np.nanstd(X)
 
-def find_outliers
+    z_score = (X - mean) / std
+    
+    return z_score
+    
+def find_outliers(X, threshold = 3):
+    z_score = get_z_score(X)
+
+    low = np.where(z_score < -threshold)[0]
+    high = np.where(z_score > threshold)[0]
+    outliers = np.concatenate((low, high))
+
+    return outliers
 
 if __name__ == '__main__':
     import generator as gen
     import matplotlib.pyplot as plt
-    func = {"type":"linear","args":[3,1]}
-    N = 10
 
-    dataline,data = gen.np_gen_func_line(func,N,10,False)
+    data = gen.get_data(func={"type":"root","args":[2.6667,0.45,-3.2]},length=100,scaling=10,mode_noise=1,strength=0.1,sleek=10,mode_miss=11,count=10,seed_noise=111,seed_miss=993)
+    dataline = data["dataline"]
 
-    dataline_miss,data = gen.get_mis(dataline,data,11,round(N/10))
+    outliers = find_outliers(dataline, 1)
 
+    print(outliers, type(outliers))
 
-    print(dataline)
-    mean = np.nanmean(dataline)
-    std = np.nanstd(dataline)
-    top = mean + std * 3
-    bot = mean - std * 3
-    print(std, mean)
+    gen.draw_data(data, 1)
 
-    plt.plot(dataline)
-    plt.axhline(y=top, color='r', linestyle='-')
-    plt.axhline(y=bot, color='r', linestyle='-')
-    print(data)
-    plt.show()
