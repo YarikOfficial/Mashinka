10d5e977d6c974584fa63b5f5c671c2a0e02c4a4
Ярослав <yarik.official@ya.ru>
2026-03-18 00:03:32 +0700

    Реализовал методы МО для обнаружения выбросв

--- analyzer.py
+++ analyzer.py
@@ -1,7 +1,8 @@
 import numpy as np
-import pandas as pd
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
 
-def get_z_score(X: np.ndarray) -> np.ndarray:
+def zscore_gen(X: np.ndarray) -> np.ndarray:
     """
     Генерирует Z-Score из массива данных.\n
     Получает: X - Данные\n
@@ -15,7 +16,7 @@ def get_z_score(X: np.ndarray) -> np.ndarray:
     
     return z_score
     
-def find_outliers(X: np.ndarray, threshold:float = 3.0, split:int = 1) -> np.ndarray:
+def zscore_find_outliers(X: np.ndarray, threshold:float = 3.0, split:int = 1) -> np.ndarray:
     """
     Находит выбросы в массиве данных с помощью метода "3-х Сигм" (Z-Score)\n
     Получает:\n
@@ -30,9 +31,9 @@ def find_outliers(X: np.ndarray, threshold:float = 3.0, split:int = 1) -> np.nda
     outliers = X != X
     for i in range(split):
         if i == split - 1:
-            z_score = get_z_score(X[i * pl:])
+            z_score = zscore_gen(X[i * pl:])
         else:
-            z_score = get_z_score(X[i * pl: (i + 1) * pl])
+            z_score = zscore_gen(X[i * pl: (i + 1) * pl])
 
         low = z_score < -threshold
         high = z_score > threshold
@@ -40,14 +41,54 @@ def find_outliers(X: np.ndarray, threshold:float = 3.0, split:int = 1) -> np.nda
 
     return outliers
 
+def get_X(data:dict) -> np.ndarray:
+    """Возвращает непрерывный массив данных X из *мегасловаря* им. Радомира!"""
+    X = data["dataline_miss"]
+    X = X.reshape(-1,1)
+    return X
+
+
+def ml_isoforest(X: np.ndarray, contamination:float = 0.001, random_state:int = 42) -> np.ndarray:
+    """
+    Находит выбросы в массиве данных с помощью метода Isolation Forest\n
+    Получает:\n
+        X - Данные\n
+        contamination - Ожидаемый процент выбросов\n
+        random_state - Случайный сид\n
+    Возвращает:\n
+        Маску выбросов\n
+    """
+
+    iso_forest = IsolationForest(contamination=contamination, random_state=random_state)
+    outlier_labels = iso_forest.fit_predict(X)
+
+    return outlier_labels == -1
+
+def ml_lof(X: np.ndarray, n_neighbors:int=5):
+    """
+    Находит выбросы в массиве данных с помощью метода Local Outlier Factor\n
+    Получает:\n
+        X - Данные\n
+        n_neighbors - Количество ближайших соседей для вычисления LOF\n
+    Возвращает:\n
+        Маску выбросов\n
+    """
+
+    lof = LocalOutlierFactor(n_neighbors=n_neighbors)
+    outlier_labels = lof.fit_predict(X)
+
+    return outlier_labels == -1
+
+
 if __name__ == '__main__':
     import generator as gen
     import matplotlib.pyplot as plt
 
-    data = gen.get_data(func={"type":"linear","args":[2,0]},length=100,scaling=10,mode_noise=2,strength=1.5,sleek=100,mode_miss=11,count=1,seed_noise=111,seed_miss=993)
-    dataline = data["dataline_miss"]
+    data = gen.get_data(func={"type":"sin","args":[2,0]},length=100,scaling=10,mode_noise=2,strength=0.1,sleek=100,mode_miss=11,count=5,seed_noise=111,seed_miss=993)
 
-    outliers = find_outliers(dataline, threshold=3, split=5)
-    print(type(outliers))
+    X = get_X(data)
+    #outliers = ml_isoforest(X, 0.001)
+    outliers = ml_lof(X)
+
     gen.draw_data(data, 1, outliers)
 
